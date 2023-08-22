f4e4a1fdfa819dc8dba7cd48818c96f71c75d45e
zainmahmoud <zeakmahm@uwaterloo.ca>
2023-08-21 23:11:02 -0400

    Converted into a web app

---
+++ WebScraper/scraper.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+import requests
+
+def get_laptop_data():
+    laptops_data = []
+
+    html_text = requests.get("https://www.canadacomputers.com/promotions/back-to-school-2023")
+    soup = BeautifulSoup(html_text.content, 'html.parser')
+
+    laptop_divs = soup.find_all('div', class_='col-12 productImageDesc')
+
+    for laptop_div in laptop_divs:
+        # Extract laptop name and link
+        laptop_name_link = laptop_div.find('a', class_='text-dark text-truncate_3')
+        laptop_name = laptop_name_link.text
+        laptop_link = laptop_name_link['href']
+
+        # Extract initial and final price
+        initial_price_span = laptop_div.find('span', class_='d-sm-block line-height').find('s')
+        initial_price = initial_price_span.get_text(strip=True) if initial_price_span else None
+
+        final_price_span = laptop_div.find('span', class_='text-danger d-block mb-0 pq-hdr-product_price line-height')
+        final_price = final_price_span.strong.get_text(strip=True) if final_price_span else None
+
+        # Extract savings
+        savings_tooltip = laptop_div.find('div', class_='d-inline-block pt-0_5')
+        savings = savings_tooltip.get_text(strip=True).replace("SAVE", "").replace("$", "")
+
+        # Define the js_code variable with your JavaScript code
+        js_code = f"""
+        <script>
+            // JavaScript code specific to this laptop
+            // For example, you can add a button click event
+            document.addEventListener('DOMContentLoaded', function() {{
+                var button = document.querySelector('#{laptop_name}-button');
+                if (button) {{
+                    button.addEventListener('click', function() {{
+                        alert('You clicked the button for {laptop_name}!');
+                    }});
+                }}
+            }});
+        </script>
+        """
+
+        laptops_data.append({
+            'name': laptop_name,
+            'link': laptop_link,
+            'initial_price': initial_price,
+            'final_price': final_price,
+            'savings': savings,
+            'js_code': js_code  # Include the JavaScript code for this laptop
+        })
+
+    return laptops_data
---
+++ run.py
@@ -0,0 +1,4 @@
+from WebApp.app import app
+
+if __name__ == '__main__':
+    app.run()
